13953fec33fede267cb08d317abf53e98d30e0af
Albert Kim <ahkim87@gmail.com>
2013-09-30 16:01:25 -0400

    first commit

---
+++ classEx/jsExamples.js
@@ -0,0 +1,15 @@
+var dogFoodGiraffeFace = document.getElementById("an-id");
+dogFoodGiraffeFace.addEventListener
+
+var aTagsInGiraffeFace = dogFoodGiraffeFace.getElementsByTagName("a");
+
+var barfBarfBarfBarf = document.getElementsByTagName("a");
+
+for(var i = 0; i < barfBarfBarfBarf.length; i++ ){
+	
+}
+
+var byClass = document.getElementByClassName("a-classname"); //
+
+var whoa = document.querySelectorAll(".a-classname > p > a");
+var whee = document.querySelector(".a-classname > p > a");
