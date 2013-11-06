d7aa5c0293585e34b4cc9ef926ad3ef5d36c62c6
Albert Kim <ahkim87@gmail.com>
2013-11-06 18:05:42 -0500

    week 11

---
+++ wk10homework/pages/brain.php
@@ -0,0 +1,22 @@
+<?php include('../header.php')?>
+
+<div id="page-content">
+
+	<ul>
+	<li><img src="work/brain.jpg"/></li>
+	</ul>
+
+	<h1 class="title">Brain Mirror</h1>
+	<p class="description">How does our state of mind affect our own self-awareness? With an EEG hooked to an Arduino, we were able to retrieve EEG brain waves and use the data to interact with the user and manipulate the image in front of them. Using the camera and a particle interface, the user's concentration and meditation values would dictate the representation of the user's facial image.<br><br>Project members: YiNing Huang, Francesca Castelli<br><br>Materials: Arduino, EEG, Processing, OpenFrameWorks</p>
+
+
+
+</div>
+
+
+
+
+
+</body>
+
+</html>
---
+++ wk10homework/pages/space.php
@@ -0,0 +1,21 @@
+<?php include('../header.php')?>
+
+<div id="page-content">
+	<ul>
+	<li><img src="work/planet.jpg"/></li>
+	</ul>
+
+	<h1 class="title">Brain Mirror</h1>
+	<p class="description">How does our state of mind affect our own self-awareness? With an EEG hooked to an Arduino, we were able to retrieve EEG brain waves and with the data use it to interact with the user. Using the camera and a particle interface, the user's concentration and meditation values would dictate the representation of the user's face.<br><br>Materials: Arduino, EEG, Processing, OpenFrameWorks</p>
+
+
+
+</div>
+
+
+
+
+
+</body>
+
+</html>
